816fde96eb5041bd31dea4dda4793492640fbb22
山田ゴンザレス花子 <nekops49@gmail.com>
2022-09-24 04:38:36 +0900

    test

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Hello-world!","page":"Hello world!","title":"Hello world!","text":"","category":"section"},{"location":"","page":"Hello world!","title":"Hello world!","text":"Hello World!","category":"page"}]
+}
