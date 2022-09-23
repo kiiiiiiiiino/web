91ded4bfbfdcf7e9ac08a08c621580048397257a
山田ゴンザレス花子 <nekops49@gmail.com>
2022-09-24 06:06:15 +0900

    変更！

--- docs/build/search_index.js
+++ docs/build/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"Chapter1/c1/#chap.1","page":"chap.1","title":"chap.1","text":"","category":"section"},{"location":"#おテスト","page":"Index","title":"おテスト","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"うんち","category":"page"},{"location":"Chapter2/c2/#chap.2","page":"chap.2","title":"chap.2","text":"","category":"section"}]
+[{"location":"chapter2/c2/#chap.2","page":"chap.2","title":"chap.2","text":"","category":"section"},{"location":"chapter1/c1/#chap.1","page":"chap.1","title":"chap.1","text":"","category":"section"},{"location":"chapter/c2/#chap.2","page":"chap.2","title":"chap.2","text":"","category":"section"},{"location":"chapter/c1/#chap.1","page":"chap.1","title":"chap.1","text":"","category":"section"},{"location":"#おテスト","page":"Index","title":"おテスト","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"うんち","category":"page"}]
 }
